59fbe8689c9fe0e7b2ac7a7d305226af26c5797f
Aarzoo <iaarzoo40@gmail.com>
2026-09-24 12:54:38 +0530

    FIX : dashboard page error

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -15,9 +15,15 @@ import {
 } from "@/components/ui/sidebar"
 
 export default function Page() {
+  const dummyUser = {
+    name: "User",
+    email: "user@example.com",
+    avatar: "",
+  };
+
   return (
     <SidebarProvider>
-      <AppSidebar />
+      <AppSidebar user={dummyUser} />
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
